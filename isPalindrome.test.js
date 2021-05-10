72989e90f5ef37b5e0a25dbc9380475025c327fa
Abdumalik Khojimirzaev <hojimirzaevabdumalik@gmail.com>
2021-05-10 14:13:46 +0500

    isPalindrome function tested

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -1,9 +1,16 @@
 const isPalindrome = require('./isPalindrome');
 
+// isPalindrome function exists
 test('isPalindrome function exists', () => {
   expect(isPalindrome).toBeDefined();
 });
 
+// Checking racecar for palindrome and return true
 test("isPalindrome('racecar') return true", () => {
   expect(isPalindrome('racecar')).toBeTruthy();
+});
+
+// Checking hello for palindrome and return false
+test("isPalindrome('hello') return false", () => {
+  expect(isPalindrome('hello')).toBeFalsy();
 });
